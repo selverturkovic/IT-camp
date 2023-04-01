9f2ecacf0df6b9f773389c6b65d8b1c5c24ac18c
selverturkovic <selvernp95@gmail.com>
2023-04-01 11:02:51 +0200

    domaci

--- JavaScript/4. Cas/app.js
+++ JavaScript/4. Cas/app.js
@@ -37,3 +37,13 @@ if (y === 0) {
 // ako je broj godina izmedju 12(ukljucuje) i 18 (ne ukljucuje): Vi ste maloletni
 // ako je broj godina izmedju 18(ukljucuje) i 40 (ne ukljucuje): vi ste punoletni
 // ako je broj godina veci ili jednak 40: vi ste zrela osoba
+
+if (brojGodina < 12) {
+  console.log("Vi ste decijeg doba");
+} else if (brojGodina >= 12 && brojGodina < 18) {
+  console.log("Vi ste maloletni");
+} else if (brojGodina >= 18 && brojGodina < 40) {
+  console.log("Vi ste punoletni");
+} else {
+  console.log("Vi ste zrela osoba");
+}
