3543a2c1d776df62bd75daf71b36f4573b9b63d2
selverturkovic <selvernp95@gmail.com>
2023-05-15 21:19:20 +0200

    17. Cas stringovi

---
+++ JavaScript/17. Cas/app.js
@@ -0,0 +1,11 @@
+// trim() metoda brise prazan prostor sa obe strane stringa.
+
+const str = "     email@gmail.com  ";
+const duzina = str.length;
+console.log(str);
+console.log(duzina);
+
+const str2 = str.trim();
+const duzina2 = str2.length;
+console.log(str2);
+console.log(duzina2);
