35e5c0cab716819aecbf8e1df30c16f96fa4aced
selverturkovic <selvernp95@gmail.com>
2023-05-24 19:38:07 +0200

    20. Cas

---
+++ JavaScript/21. Cas/domaci.js
@@ -0,0 +1,8 @@
+// Domaci zadaci:
+// Napraviti funkciju koja ispituje argument.
+// Ako se u recenici nadje rec "skola", neka se vrati pozicija prvog pojavljivanja date reci.
+// Ako recenica pocinje sa recju "Automobil" neka se vrati boolean true;
+// Ako je recenica parne duzine neka se vrati broj pojavljivanja reci "kuca".
+// Inace vratiti duzinu datog stinga.
+
+// 3.	Prebrojati koliko ima malih slova u unetom stringu. Slova koja se koriste u stringu pripadaju isključivo engleskoj abecedi.
