38eb2ad035a68f0d78a1b15c7975db5758bbf075
selverturkovic <selvernp95@gmail.com>
2023-09-22 17:48:49 +0200

    react

---
+++ React/2. Cas/index.js
@@ -0,0 +1,57 @@
+// promises
+// event loop stack queue and web api video
+// async await
+// DOM i BOM  //javascript objekti dom i bom
+// DOM manipulation
+// events
+
+// teorija
+// closure
+// hoisting
+// classes
+// es6 js ecma script nova verzija java scripta
+// http i fetch
+
+// prompt("dfslf");
+
+// console.log(first);
+
+// setTimeout(() => {
+//   console.log(nesto);
+// });
+
+// const promise = new Promise((resolve, reject) => {
+//   console.log("Promise"); // sinhrono
+//   resolve("text"); // na webapi da se desava ansihrono
+// });
+// promise.then(() => {
+//   //asihrona operacija desava se kasnije jsv9000.app callstack je javascript
+//   console.log(text);
+// });
+// const func = async () => {
+//   // kod je citljiviji na ovaj nacin
+//   try {
+//     const result = await fetch();
+//   } catch (error) {}
+// };
+// document manipulacija
+// bom global scope da se rade razlicite stvari
+// console.log(window.document);
+// console.log(document);
+
+// const element = document.getElementById("NekiE");
+// console.log(element);
+
+// element.addEventListner //click event
+// function declaration hoista se, expression ne hoista
+// var global scope
+// const i let nisu global scope
+// temporary dead zone
+// klase recepti za pravljenje objekata
+// object oriented programing backend
+// api rest api
+//http
+// javascript.info
+// knjiga you dont know JS Yet github
+// git and github tutorial
+// clean code
---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ display: "flex", gap: 20 }}>
+      <p
+        style={{
+          width: "100%",
+          fontSize: 34,
+          fontWeight: 700,
+          padding: 0,
+          margin: 0,
+        }}
+      >
+        Kickstarter Actually printerest brunch bitters ocupy
+      </p>
+
+      <div style={{ width: "100%" }}>
+        <p style={{ width: "100%" }}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus,
+          fugit?
+        </p>
+        <div style={{ display: "flex", gap: 10, alignItems: "center" }}>
+          <button>Button</button> <a href="">Learn more</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
